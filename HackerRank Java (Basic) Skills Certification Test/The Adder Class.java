77feea580482a023e4ac72cfe956faa2411f773d
Aman Eleven <hackerr647@gmail.com>
2022-03-13 14:28:45 +0530

    Create The Adder Class.java

---
+++ HackerRank Java (Basic) Skills Certification Test/The Adder Class.java
@@ -0,0 +1,13 @@
+//only write this code and Run the test cases 
+
+
+class Adder extends Calculator{
+     
+     int add(int a, int b){
+         return a+b;
+     
+//Author : AmanEleven
+//Follow : All Personal and Professional Links Here -->  https://linktr.ee/amaneleven
+       
+     }
+ }
